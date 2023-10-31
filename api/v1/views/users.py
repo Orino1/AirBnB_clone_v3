ac114d88601437295f1b1515210e91c795db7990
walid <jabiriwalid555@gmail.com>
2023-10-31 21:08:37 +0100

    editing files

--- api/v1/views/users.py
+++ api/v1/views/users.py
@@ -1,92 +1,53 @@
 #!/usr/bin/python3
-"""users"""
+"""
+users
+"""
+
 from api.v1.views import app_views
-from flask import jsonify, abort, request
 from models import storage
+from flask import abort, jsonify, request
 from models.user import User
-from datetime import datetime
-import uuid
 
 
-@app_views.route('/users/', methods=['GET'])
-@app_views.route('/users', methods=['GET'])
-def list_users():
-    '''Retrieves a list of all User objects'''
-    list_users = [obj.to_dict() for obj in storage.all("User").values()]
-    return jsonify(list_users)
+@app_views.route('/users', methods=['GET', 'POST'])
+def users():
+    """
+    Flask route
+    """
+    if request.method == 'POST':
+        kwargs = request.get_json()
+        if not kwargs:
+            return {"error": "Not a JSON"}, 400
+        if "email" not in kwargs:
+            return {"error": "Missing email"}, 400
+        if "password" not in kwargs:
+            return {"error": "Missing password"}, 400
+        new_user = User(**kwargs)
+        new_user.save()
+        return new_user.to_dict(), 201
 
+    elif request.method == 'GET':
+        return jsonify([u.to_dict() for u in storage.all("User").values()])
 
-@app_views.route('/users/<user_id>', methods=['GET'])
-def get_user(user_id):
-    '''Retrieves a User object'''
-    all_users = storage.all("User").values()
-    user_obj = [obj.to_dict() for obj in all_users if obj.id == user_id]
-    if user_obj == []:
-        abort(404)
-    return jsonify(user_obj[0])
-
-
-@app_views.route('/users/<user_id>', methods=['DELETE'])
-def delete_user(user_id):
-    '''Deletes a User object'''
-    all_users = storage.all("User").values()
-    user_obj = [obj.to_dict() for obj in all_users if obj.id == user_id]
-    if user_obj == []:
-        abort(404)
-    user_obj.remove(user_obj[0])
-    for obj in all_users:
-        if obj.id == user_id:
-            storage.delete(obj)
+@app_views.route('/users/<id>', methods=['GET', 'DELETE', 'PUT'])
+def users_id(id):
+    """
+    Flask route
+    """
+    user = storage.get(User, id)
+    if (user):
+        if request.method == 'DELETE':
+            user.delete()
             storage.save()
-    return jsonify({}), 200
+            return {}, 200
 
-
-@app_views.route('/users/', methods=['POST'])
-def create_user():
-    '''Creates a User'''
-    if not request.get_json():
-        abort(400, 'Not a JSON')
-    if 'email' not in request.get_json():
-        abort(400, 'Missing name')
-    if 'password' not in request.get_json():
-        abort(400, 'Missing name')
-    users = []
-    new_user = User(email=request.json['email'],
-                    password=request.json['password'])
-    storage.new(new_user)
-    storage.save()
-    users.append(new_user.to_dict())
-    return jsonify(users[0]), 201
-
-
-@app_views.route('/users/<user_id>', methods=['PUT'])
-def updates_user(user_id):
-    '''Updates a User object'''
-    all_users = storage.all("User").values()
-    user_obj = [obj.to_dict() for obj in all_users if obj.id == user_id]
-    if user_obj == []:
-        abort(404)
-    if not request.get_json():
-        abort(400, 'Not a JSON')
-    try:
-        user_obj[0]['first_name'] = request.json['first_name']
-    except:
-        pass
-    try:
-        user_obj[0]['last_name'] = request.json['last_name']
-    except:
-        pass
-    for obj in all_users:
-        if obj.id == user_id:
-            try:
-                if request.json['first_name'] is not None:
-                    obj.first_name = request.json['first_name']
-            except:
-                pass
-            try:
-                if request.json['last_name'] is not None:
-                    obj.last_name = request.json['last_name']
-            except:
-                pass
-    storage.save()
-    return jsonify(user_obj[0]), 200
+        elif request.method == 'PUT':
+            kwargs = request.get_json()
+            if not kwargs:
+                return {"error": "Not a JSON"}, 400
+            for k, v in kwargs.items():
+                if k not in ["id", "email", "created_at", "updated_at"]:
+                    setattr(user, k, v)
+            user.save()
+        return user.to_dict()
+    abort(404)
